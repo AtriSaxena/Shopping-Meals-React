ea93791bf5fae9d3bc360365f8a954afd0695660
Atri Saxena <atrisaxena2@gmail.com>
2021-05-13 00:32:13 +0530

    meal [roject

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './components/App';
+import registerServiceWorker from './registerServiceWorker';
+import { createStore } from 'redux';
+import reducer from './reducers';
+
+const store = createStore(reducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    )
+
+console.log(store)
+ReactDOM.render(<App store={store}/>, document.getElementById('root'));
+registerServiceWorker();
